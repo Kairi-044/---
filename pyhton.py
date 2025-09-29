d30062d73b8f2ab9891ddcc3fdb99cc6a7601e4c
user1 <kovalenkovadim728@gmail.com>
2025-09-29 12:16:01 +0300

    Не рабочие диагонали

--- pyhton.py
+++ pyhton.py
@@ -22,36 +22,45 @@ def main():
 
                 is_finished = False   
                 desk=["_","_","_","_","_","_","_","_","_"]
-                while not is_finished:
-                    clear_screen()
+                while not (is_finished) and ("_" in desk):
+
                    
 
                     print(desk[0],desk[1],desk[2])
                     print(desk[3],desk[4],desk[5])
                     print(desk[6],desk[7],desk[8])
-                   
+                    turn=int(input("Введите клетку: "))
+                    while desk[turn-1] != "_":
+                        print("клетка занята")
+                        turn=int(input("Повторно введите клетку: "))
                     if turns_counter %2==1:
-                        turn=int(input("Введите клетку: "))
-                        desk[turn-1]="X"
-                        x_turns.append(turn-1)
+                            desk[turn-1]="X"
+                            x_turns.append(turn-1)
+                           
                     else:
-                        turn=int(input("Введите клетку: "))
-                        desk[turn-1]="0"
-                        o_turns.append(turn-1)
+                            desk[turn-1]="0"
+                            o_turns.append(turn-1)
+                        
+                 
+                
                     x_win = check_win(x_turns)
                     o_win = check_win(o_turns)
                     is_finished = x_win or o_win 
                     turns_counter+=1
-                clear_screen()
+                    print(x_win,o_win)
                 print(desk[0],desk[1],desk[2])
                 print(desk[3],desk[4],desk[5])
                 print(desk[6],desk[7],desk[8])
                 if x_win:
                     print("Победили X")
-                else:
+                elif o_win:
                     print("Победили 0")
+                else:
+                    print("Ничья")
+                
 
-                 
+
+
 
 
 
